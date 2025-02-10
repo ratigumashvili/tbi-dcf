de91a1e778338e73f2183013bc8cd9fa5f4ff932
ratigumashvili <ratigumashvili@gmail.com>
2025-02-10 16:20:17 +0400

    cleanup

--- src/app/(home)/page.js
+++ src/app/(home)/page.js
@@ -1,15 +1,7 @@
-import { requireUser } from "@/app/_lib/helpers"
-
 export default async function HomePage() {
-  const { data, status } = await requireUser()
   return (
     <div>
-      <pre>
-        {JSON.stringify(data, null, 2)}
-      </pre>
-      <pre>
-        {JSON.stringify(status, null, 2)}
-      </pre>
+      home page
     </div>
   )
 
